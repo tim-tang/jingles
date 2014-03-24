5e169e417f5dc4010857d1bba9eb94db58b5bdf8
Heinz N. Gies <heinz@licenser.net>
2014-03-24 18:05:47 +0100

    Updated help.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,34 +6,38 @@ var wait = {
   }]
 }
 
+function help_url(page, section) {
+    return "https://docs.project-fifo.net/jingles/" + page +".html#" + section;
+}
+
 angular.module('fifoApp',
-  ['ngRoute', 'ngAnimate', 'ngResource','services.breadcrumbs', 
+  ['ngRoute', 'ngAnimate', 'ngResource','services.breadcrumbs',
   'gettext', 'infinite-scroll', 'ngTable', 'angularMoment', 'ngSanitize', 'ngCookies', 'angularFileUpload'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Dashboard',
+        helpUrl: help_url('general', 'dashboard'),
         resolve: wait
       })
       .when('/machines', {
         templateUrl: 'views/machines.html',
         controller: 'MachinesCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Machines+list',
+        helpUrl: help_url('machines', 'list'),
         name: 'Machines',
         resolve: wait
       })
       .when('/datasets', {
         templateUrl: 'views/datasets.html',
         controller: 'DatasetsCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Datasets',
+        helpUrl: help_url('datasets', 'installed'),
         name: 'Datasets'
       })
       .when('/servers', {
         templateUrl: 'views/servers.html',
         controller: 'ServersCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Hypervisors',
+        helpUrl: help_url('hypervisors', 'list'),
         name: 'Servers'
       })
       .when('/configuration', {
@@ -42,137 +46,143 @@ angular.module('fifoApp',
       .when('/configuration/packages', {
         templateUrl: 'views/packages.html',
         controller: 'PackagesCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Packages',
+        helpUrl: help_url('packages', 'list'),
         name: 'Packages'
       })
       .when('/configuration/networks/new', {
         templateUrl: 'views/network-new.html',
         controller: 'NetworkNewCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Networks'
+        helpUrl: help_url('network', 'new')
       })
       .when('/configuration/networks', {
         templateUrl: 'views/networks.html',
         controller: 'NetworksCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Networks',
+        helpUrl: help_url('network', 'list'),
         name: 'Networks'
       })
       .when('/configuration/ip-ranges', {
         templateUrl: 'views/ip-ranges.html',
         controller: 'IpRangesCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Ipranges',
+        helpUrl: help_url('ipranges', 'list'),
         name: 'IP Ranges'
       })
       .when('/configuration/users', {
         templateUrl: 'views/users.html',
         controller: 'UsersCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Users',
+        helpUrl:  help_url('user', 'list'),
         name: 'Users'
       })
       .when('/configuration/groups', {
         templateUrl: 'views/groups.html',
         controller: 'GroupsCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Groups',
+        helpUrl:  help_url('group', 'list'),
         name: 'Groups'
       })
       .when('/configuration/organizations', {
         templateUrl: 'views/organizations.html',
         controller: 'OrganizationsCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Orgs',
+        helpUrl:  help_url('orgs', 'list'),
         name: 'Organizations'
       })
       .when('/configuration/dtraces', {
         templateUrl: 'views/dtraces.html',
         controller: 'DtracesCtrl',
         name: 'Tracing',
-        helpUrl: 'http://project-fifo.net/display/PF/DTrace'
+        helpUrl:  help_url('dtraces', 'list')
       })
       .when('/machines/new', {
         templateUrl: 'views/machine-new.html',
         controller: 'MachineNewCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Machine+Details',
+        helpUrl: help_url('machines', 'new')
       })
       .when('/machines/:uuid', {
         templateUrl: 'views/machine.html',
         controller: 'MachineCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Machine+Details',
+        helpUrl: help_url('machines', 'details')
       })
       .when('/datasets/:uuid', {
         templateUrl: 'views/dataset.html',
-        helpUrl: 'http://project-fifo.net/display/PF/Datasets',
+        helpUrl: help_url('datasets', 'details'),
         controller: 'DatasetCtrl',
       })
       .when('/servers/:uuid', {
         templateUrl: 'views/server.html',
-        helpUrl: 'http://project-fifo.net/display/PF/Hypervisors',
+        helpUrl: help_url('hypervisors', 'details'),
         controller: 'ServerCtrl'
       })
       .when('/configuration/packages/new', {
         templateUrl: 'views/package-new.html',
-        helpUrl: 'http://project-fifo.net/display/PF/Packages',
+        helpUrl: help_url('packages', 'new'),
         controller: 'PackageNewCtrl'
       })
       .when('/configuration/packages/:uuid', {
         templateUrl: 'views/package.html',
-        helpUrl: 'http://project-fifo.net/display/PF/Packages',
+        helpUrl: help_url('packages', 'details'),
         controller: 'PackageCtrl'
       })
       .when('/configuration/networks/:uuid', {
         templateUrl: 'views/network.html',
-        helpUrl: 'http://project-fifo.net/display/PF/Networks',
+        helpUrl: help_url('network', 'details'),
         controller: 'NetworkCtrl'
       })
       .when('/configuration/users/new', {
         templateUrl: 'views/user-new.html',
-        helpUrl: 'http://project-fifo.net/display/PF/Users',
+        helpUrl:  help_url('user', 'create'),
         controller: 'UserNewCtrl'
       })
       .when('/configuration/users/:uuid', {
         templateUrl: 'views/user.html',
         controller: 'UserCtrl',
-        helpUrl: 'http://project-fifo.net/display/PF/Users'
+        helpUrl:  help_url('user', 'details')
       })
       .when('/about', {
         templateUrl: 'views/about.html',
+        helpUrl:  help_url('general', 'about'),
         controller: 'AboutCtrl'
       })
       .when('/configuration/groups/new', {
         templateUrl: 'views/group-new.html',
-        helpUrl: 'http://project-fifo.net/display/PF/Groups',
+        helpUrl:  help_url('group', 'create'),
         controller: 'GroupNewCtrl'
       })
       .when('/configuration/groups/:uuid', {
         templateUrl: 'views/group.html',
-        helpUrl: 'http://project-fifo.net/display/PF/Groups',
+        helpUrl:  help_url('group', 'details'),
         controller: 'GroupCtrl'
       })
       .when('/configuration/organizations/new', {
         templateUrl: 'views/organization-new.html',
+        helpUrl:  help_url('orgs', 'create'),
         controller: 'OrganizationNewCtrl'
       })
       .when('/configuration/organizations/:uuid', {
         templateUrl: 'views/organization.html',
+        helpUrl:  help_url('orgs', 'details'),
         controller: 'OrganizationCtrl'
       })
       .when('/configuration/dtraces/new', {
         templateUrl: 'views/dtrace-new.html',
-        helpUrl: 'http://project-fifo.net/display/PF/DTrace',
+        helpUrl:  help_url('dtraces', 'new'),
         controller: 'DtraceNewCtrl'
       })
       .when('/configuration/dtraces/:uuid', {
         templateUrl: 'views/dtrace.html',
-        helpUrl: 'http://project-fifo.net/display/PF/DTrace',
+        helpUrl:  help_url('dtraces', 'details'),
         controller: 'DtraceCtrl'
       })
       .when('/login', {
         templateUrl: 'views/login.html',
+        helpUrl:  help_url('general', 'login'),
         controller: 'LoginCtrl',
       })
       .when('/configuration/ip-ranges/new', {
         templateUrl: 'views/ip-range-new.html',
+        helpUrl: help_url('ipranges', 'new'),
         controller: 'IpRangeNewCtrl'
       })
       .when('/visualizations/graph', {
         templateUrl: 'views/vis-graph.html',
+        helpUrl:  help_url('general', 'cloudview'),
         controller: 'VisGraphCtrl'
       })
       .otherwise({
