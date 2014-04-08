d83dcb0ec91f0848802ac709cad2624f9707e055
Phillip Neumann <pneumann@gmail.com>
2014-04-08 11:24:58 -0300

    For remote datasets(.at), by default point to https instead of http, so firefox does not complain about "MixedContent".

--- app/scripts/services/datasetsat.js
+++ app/scripts/services/datasetsat.js
@@ -3,15 +3,21 @@
 angular.module('fifoApp')
   .factory('datasetsat', function ($resource, $http) {
  
-    var endpoint = "http://" + Config.datasets + "/"
+    var endpoint = Config.datasets
+
+    //If the config does not have a http(s), add https as default
+    if (endpoint.indexOf('http') != 0)
+        endpoint = 'https://' + endpoint
+
     var services = {
-        datasets: $resource(endpoint + 'datasets/:id',
+        endpoint: endpoint,
+        datasets: $resource(endpoint + '/datasets/:id',
                             {id: '@id'}),
     }
 
     /* Gets with cache! */
     services.datasets.get = function(obj, success, error) {
-        res = $http.get(endpoint + 'datasets/' + obj.id, {cache: true})
+        res = $http.get(endpoint + '/datasets/' + obj.id, {cache: true})
             .success(success)
             .error(function(data) {
                 error && error(data)
